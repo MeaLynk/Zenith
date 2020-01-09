ad96d12f99a8be35c09c89e48691c11867626ca6
Kameron <47528324+MeaLynk@users.noreply.github.com>
2020-01-09 14:05:10 -0500

    Orbs spawn, move now

--- Zenith/Assets/Zenith_Files/Scripts/Player/Orb Scripts/RemoteOrbMine.cs
+++ Zenith/Assets/Zenith_Files/Scripts/Player/Orb Scripts/RemoteOrbMine.cs
@@ -2,7 +2,7 @@
 //  Remote Orb Throw Script
 //  By: Kameron Brodhagen
 //
-//  Script attached to the orb keeping track of it
+//  Script attached to the orb keeping track of it's status
 //--------------------------------------------------------------
 using System.Collections;
 using System.Collections.Generic;
@@ -13,16 +13,54 @@ public class RemoteOrbMine : MonoBehaviour
     public enum Mode { PUSH, PULL }
 
     public Mode currentMode;
+    public float throwTimer = 10;
+    public float activeTimer = 5;
+
+    [HideInInspector]
+    public RemoteOrbThrow parent;
+
+    private bool isActive = false;
+    private float currentTimer = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentTimer = throwTimer; //How long the orb has in order to hit something (in order to prevent them from staying in scene if nothing is hit)
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(isActive == true)
+        {
+
+        }
+
+        if(currentTimer > 0)
+        {
+            currentTimer -= Time.deltaTime;
+        }
+        else
+        {
+            parent.hasBeenFired = false; //Resets throw
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Orb-able Area")
+        {
+            Debug.Log("Orb Successfully Activated.");
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            isActive = true;
+            currentTimer = activeTimer; //Resets timer for active time legnth
+        }
+        else
+        {
+            Debug.Log("Orb Failed to Activate.");
+            parent.hasBeenFired = false; //Resets throw
+            Destroy(gameObject);
+        }
     }
 }
--- Zenith/Assets/Zenith_Files/Scripts/Player/Orb Scripts/RemoteOrbThrow.cs
+++ Zenith/Assets/Zenith_Files/Scripts/Player/Orb Scripts/RemoteOrbThrow.cs
@@ -12,36 +12,53 @@ public class RemoteOrbThrow : MonoBehaviour
 {
     public GameObject pullOrbPrefab;
     public GameObject pushOrbPrefab;
+    public Transform orbEmitter;
+    public float throwSpeed = 5.0f;
 
-    private bool hasBeenPressed = false; //NOO HOLD
+    [HideInInspector]
+    public bool hasBeenFired = false;
+
+    private Transform mainCameraTrans;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainCameraTrans = GetComponentInChildren<Camera>().transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButton("PullOrb"))
+        //TODO: Add stanima check/update when stanima is put into it's own script
+
+        if (Input.GetButton("PullOrb"))
         {
-            if(hasBeenPressed == false)
+            if(hasBeenFired == false)
             {
                 SpawnOrb(pullOrbPrefab);
-                hasBeenPressed = true;
+                hasBeenFired = true;
             }
 
-            Debug.Log("PullOrb Pressed");
+            //Debug.Log("PullOrb Pressed");
         }
         else if(Input.GetButton("PushOrb"))
         {
-
+            if (hasBeenFired == false)
+            {
+                SpawnOrb(pushOrbPrefab);
+                hasBeenFired = true;
+            }
         }
     }
 
     private void SpawnOrb(GameObject orbPrefab)
     {
+        //TODO: Add delay to spawn after animation plays
 
+        GameObject newOrb = Instantiate(orbPrefab, orbEmitter.position, orbEmitter.rotation, null);
+        newOrb.GetComponent<RemoteOrbMine>().parent = this;
+
+        Vector3 dir = mainCameraTrans.forward; //Dir player is facing INCLUDING rotation
+        newOrb.GetComponent<Rigidbody>().AddForce(dir * throwSpeed, ForceMode.Impulse);
     }
 }
