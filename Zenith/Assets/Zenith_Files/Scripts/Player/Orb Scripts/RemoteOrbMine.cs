c01bde0a5efd15df123fd32782a54554a80b4493
Kameron <47528324+MeaLynk@users.noreply.github.com>
2020-01-09 13:01:06 -0500

    Merged PullPush & MineMode Progress

---
+++ Zenith/Assets/Zenith_Files/Scripts/Player/Orb Scripts/RemoteOrbMine.cs
@@ -0,0 +1,28 @@
+﻿//--------------------------------------------------------------
+//  Remote Orb Throw Script
+//  By: Kameron Brodhagen
+//
+//  Script attached to the orb keeping track of it
+//--------------------------------------------------------------
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoteOrbMine : MonoBehaviour
+{
+    public enum Mode { PUSH, PULL }
+
+    public Mode currentMode;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
