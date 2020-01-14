9f0bab7eccff72a04638d2ace933ed700f7d588c
Kameron <47528324+MeaLynk@users.noreply.github.com>
2020-01-14 12:41:43 -0500

    Merge For The Master

---
+++ Zenith/Assets/Zenith_Files/Scripts/LabPuzzle/LabMachine.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabMachine : MonoBehaviour
+{
+    public enum MachineType { centrifuge, compactor, combinator }
+
+    public MachineType machineType;
+
+    public Transform machineOutput;
+
+    public AudioClip machineIdleSound;
+    public AudioClip machineAltSound;
+    public AudioClip machineOperatingSound;
+
+    public float audioInterval;
+    float elapsedTime;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject newMaterial = ProcessMaterial(collision.gameObject, machineType);
+
+        if (newMaterial != null)
+        {
+            //Play AudioClip for machine operating and or particle effects
+            Destroy(collision.gameObject);
+            Instantiate<GameObject>(newMaterial, machineOutput);
+        }
+    }
+
+    /// <summary>
+    /// Functionality of the machines
+    /// </summary>
+    /// <param name="assetIn">The game object placed into the machine</param>
+    /// <param name="machine">The machine being used</param>
+    /// <returns></returns>
+    public GameObject ProcessMaterial(GameObject assetIn, MachineType machine)
+    {
+        ZenithMaterial objIn;
+
+        if (assetIn.TryGetComponent<ZenithMaterial>(out objIn))
+        {
+            if ((int)objIn.currentState == (int)machine)
+            {
+                //Material was placed in the proper machine
+                return objIn.NextState();
+            }
+            else
+            {
+                //Material was placed in the wrong machine
+                return objIn.damagedMaterial;
+            }
+        }
+        return null;
+    }
+
+    private void Update()
+    {
+        elapsedTime += Time.deltaTime;
+
+        if (elapsedTime > audioInterval)
+        {
+            elapsedTime = 0;
+
+            //Play Machine alt sound and or particle effects
+        }
+    }
+}
---
+++ Zenith/Assets/Zenith_Files/Scripts/LabPuzzle/ZenithMaterial.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ZenithMaterial : MonoBehaviour
+{
+    public enum MaterialState { original, refined, processed, damaged }
+
+    public GameObject completedMaterial;   
+    
+    public GameObject originalMaterial;
+    public GameObject refinedMaterial;
+    public GameObject processedMaterial;
+    public GameObject damagedMaterial;
+
+    public MaterialState currentState;
+
+    public GameObject NextState()
+    {
+        switch (currentState)
+        {
+            case MaterialState.original:
+                currentState++;
+                return refinedMaterial;
+
+            case MaterialState.refined:
+                currentState++;
+                return processedMaterial;
+
+            case MaterialState.processed:
+                currentState = MaterialState.damaged;
+                return damagedMaterial;
+
+            case MaterialState.damaged:
+                return damagedMaterial;
+
+            default:
+                return damagedMaterial;
+        }
+    }       
+}
